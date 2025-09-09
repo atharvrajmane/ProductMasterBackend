e24aa348694e7603e8bb41a2a14afc9c7c42f929
Atharv Rajmane <atharvrajmane81@gmail.com>
2025-09-09 19:02:17 +0530

    fix: Increase DB connection timeout and specify port

--- config/db.js
+++ config/db.js
@@ -6,11 +6,23 @@ dotenv.config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  connectTimeout: 30000,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
+
+pool
+  .getConnection()
+  .then((conn) => {
+    console.log("✅ MySQL Connection Pool Created Successfully");
+    conn.release();
+  })
+  .catch((err) => {
+    console.error("❌ Failed to Create MySQL Connection Pool:", err.message);
+  });
 
 export default pool;
