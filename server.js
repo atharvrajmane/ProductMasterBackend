69d74369d12e81cab28748ff24cab819850e623f
Atharv Rajmane <atharvrajmane81@gmail.com>
2025-09-09 18:47:21 +0530

    updated port for deployment

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ const PORT = process.env.PORT || 5000;
 
 // Start Server
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${process.env.PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
